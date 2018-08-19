1b1113998fcdc282a497e4c61c82ad5010760cfa
Wanes Missals <wanes.missals@gmail.com>
2018-08-19 23:23:59 +0300

    netatmo: handling data

--- weather/services/netatmo.py
+++ weather/services/netatmo.py
@@ -1,18 +1,19 @@
 import requests
 
+from datetime import datetime, timedelta
+
 from weather.local_settings import *
 
-payload = {
-    'grant_type': 'password',
-    'username': NETATMO_USERNAME,
-    'password': NETATMO_PASSWORD,
-    'client_id': NETATMO_CLIENT_ID,
-    'client_secret': NETATMO_CLIENT_SECRET,
-    'scope': 'read_station'
-}
+username = NETATMO_USERNAME
+password = NETATMO_PASSWORD
+client_id = NETATMO_CLIENT_ID
+client_secret = NETATMO_CLIENT_SECRET
 
 device_id = NETATMO_MAIN_DEVICE_ID
 
+
+# TODO: db interactions
+# TODO: logging
 
 class NetAtmoAPI:
 
@@ -21,13 +22,23 @@ class NetAtmoAPI:
         self.access_token = None
         self.refresh_token = None
         self.expires_in = None
+        self.expires_at = None
 
         self.authenticate()
 
     def authenticate(self):
 
+        auth_payload = {
+            'grant_type': 'password',
+            'username': username,
+            'password': password,
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'scope': 'read_station'
+        }
+
         try:
-            response = requests.post("https://api.netatmo.com/oauth2/token", data=payload)
+            response = requests.post("https://api.netatmo.com/oauth2/token", data=auth_payload)
             response.raise_for_status()
             access_token = response.json()["access_token"]
             refresh_token = response.json()["refresh_token"]
@@ -37,6 +48,8 @@ class NetAtmoAPI:
             print("Your refresh token is:", refresh_token)
             print("Your scopes are:", scope)
 
+            expires_at = datetime.now() + timedelta(seconds=expires_in)
+
         except requests.exceptions.HTTPError as error:
             print(error.response.status_code, error.response.text)
             return
@@ -44,6 +57,62 @@ class NetAtmoAPI:
         self.access_token = access_token
         self.refresh_token = refresh_token
         self.expires_in = expires_in
+        self.expires_at = expires_at
+
+    def check_and_refresh_token(self):
+
+        # Check if we should have a valid token still. If not, refresh ...
+        now = datetime.now()
+
+        if self.expires_at < now:
+            return
+        else:
+            self.refresh()
+
+    def refresh(self):
+
+        refresh_payload = {
+            'grant_type': 'refresh_token',
+            'refresh_token': None,
+            'client_id': None,
+            'client_secret': None,
+        }
+
+        try:
+            response = requests.post("https://api.netatmo.com/oauth2/token", data=refresh_payload)
+            response.raise_for_status()
+            access_token = response.json()["access_token"]
+            refresh_token = response.json()["refresh_token"]
+            expires_in = response.json()["expires_in"]
+            print("Your access token is:", access_token)
+            print("Your refresh token is:", refresh_token)
+
+            expires_at = datetime.now() + timedelta(seconds=expires_in)
+
+        except requests.exceptions.HTTPError as error:
+            print(error.response.status_code, error.response.text)
+            return
+
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        self.expires_in = expires_in
+        self.expires_at = expires_at
+
+    def q(self, url, params=None):
+
+        self.check_and_refresh_token()
+
+        try:
+            response = requests.post(url, params=params)
+            response.raise_for_status()
+            data = response.json()["body"]
+
+            return data
+
+        except requests.exceptions.HTTPError as error:
+            print(error.response.status_code, error.response.text)
+
+            return None
 
     def get_user(self):
 
@@ -82,9 +151,28 @@ class NetAtmoAPI:
 
             return None
 
+    def current(self):
+
+        data = self.get_stations_data()
+
+        for d in data['devices']:
+
+            print(d['module_name'])
+
+            # Get main unit data
+            device_data = d['dashboard_data']
+            print(device_data['Temperature'])
+            curtime = datetime.utcfromtimestamp(device_data['time_utc'])
+            print(curtime)
+
+            for m in d['modules']:
+                print(m['module_name'])
+
+                module_data = m['dashboard_data']
+                print(module_data['Temperature'])
+
 
 def get_example_user():
-
     example_response = {
         'devices': [
             {'_id': 'main_device_id',
@@ -190,66 +278,65 @@ def get_example_user():
 
 
 def get_example_station():
-
     example_response = {
-                'devices': [
+        'devices': [
+            {
+                '_id': 'main_device_id',
+                'cipher_id': 'some_weird_cipher_id',
+                'date_setup': 1516125518, 'last_setup': 1516125518, 'type': 'NAMain',
+                'last_status_store': 1534451903, 'module_name': 'Living Room', 'firmware': 134,
+                'last_upgrade': 1516125520, 'wifi_status': 36, 'co2_calibrating': False,
+                'station_name': 'Main device name',
+                'data_type': ['Temperature', 'CO2', 'Humidity', 'Noise', 'Pressure'],
+                'place': {
+                    'altitude': 6, 'city': 'Helsinki', 'country': 'FI',
+                    'timezone': 'Europe/Helsinki', 'location': [24, 60]},
+                'dashboard_data': {
+                    'time_utc': 1534451890, 'Temperature': 25.6, 'CO2': 418,
+                    'Humidity': 47, 'Noise': 43, 'Pressure': 1024.4,
+                    'AbsolutePressure': 1023.7, 'min_temp': 25.5,
+                    'max_temp': 27.3, 'date_min_temp': 1534390762,
+                    'date_max_temp': 1534440997, 'temp_trend': 'down',
+                    'pressure_trend': 'stable'
+                },
+                'modules': [
                     {
-                        '_id': 'main_device_id',
-                        'cipher_id': 'some_weird_cipher_id',
-                        'date_setup': 1516125518, 'last_setup': 1516125518, 'type': 'NAMain',
-                        'last_status_store': 1534451903, 'module_name': 'Living Room', 'firmware': 134,
-                        'last_upgrade': 1516125520, 'wifi_status': 36, 'co2_calibrating': False,
-                        'station_name': 'Main device name',
-                        'data_type': ['Temperature', 'CO2', 'Humidity', 'Noise', 'Pressure'],
-                        'place': {
-                            'altitude': 6, 'city': 'Helsinki', 'country': 'FI',
-                            'timezone': 'Europe/Helsinki', 'location': [24, 60]},
+                        '_id': 'balcony_device_id', 'type': 'NAModule1', 'module_name': 'Balcony',
+                        'data_type': ['Temperature', 'Humidity'], 'last_setup': 1516125519,
                         'dashboard_data': {
-                            'time_utc': 1534451890, 'Temperature': 25.6, 'CO2': 418,
-                            'Humidity': 47, 'Noise': 43, 'Pressure': 1024.4,
-                            'AbsolutePressure': 1023.7, 'min_temp': 25.5,
-                            'max_temp': 27.3, 'date_min_temp': 1534390762,
-                            'date_max_temp': 1534440997, 'temp_trend': 'down',
-                            'pressure_trend': 'stable'
+                            'time_utc': 1534451860, 'Temperature': 21.8, 'Humidity': 59,
+                            'min_temp': 17.6,
+                            'max_temp': 27.7, 'date_min_temp': 1534387107,
+                            'date_max_temp': 1534427661,
+                            'temp_trend': 'stable'
+                        }, 'firmware': 46, 'last_message': 1534451898,
+                        'last_seen': 1534451860, 'rf_status': 65, 'battery_vp': 5604, 'battery_percent': 84
+                    },
+                    {
+                        '_id': 'bedroom_device_id', 'type': 'NAModule4', 'module_name': 'Bedroom',
+                        'data_type': ['Temperature', 'CO2', 'Humidity'], 'last_setup': 1520184177,
+                        'dashboard_data': {
+                            'time_utc': 1534451853, 'Temperature': 24.1, 'CO2': 401, 'Humidity': 50,
+                            'min_temp': 23.8, 'max_temp': 26, 'date_min_temp': 1534390433,
+                            'date_max_temp': 1534437036, 'temp_trend': 'stable'
                         },
-                        'modules': [
-                            {
-                                '_id': 'balcony_device_id', 'type': 'NAModule1', 'module_name': 'Balcony',
-                                'data_type': ['Temperature', 'Humidity'], 'last_setup': 1516125519,
-                                'dashboard_data': {
-                                    'time_utc': 1534451860, 'Temperature': 21.8, 'Humidity': 59,
-                                    'min_temp': 17.6,
-                                    'max_temp': 27.7, 'date_min_temp': 1534387107,
-                                    'date_max_temp': 1534427661,
-                                    'temp_trend': 'stable'
-                                }, 'firmware': 46, 'last_message': 1534451898,
-                                'last_seen': 1534451860, 'rf_status': 65, 'battery_vp': 5604, 'battery_percent': 84
-                            },
-                            {
-                                '_id': 'bedroom_device_id', 'type': 'NAModule4', 'module_name': 'Bedroom',
-                                'data_type': ['Temperature', 'CO2', 'Humidity'], 'last_setup': 1520184177,
-                                'dashboard_data': {
-                                    'time_utc': 1534451853, 'Temperature': 24.1, 'CO2': 401, 'Humidity': 50,
-                                    'min_temp': 23.8, 'max_temp': 26, 'date_min_temp': 1534390433,
-                                    'date_max_temp': 1534437036, 'temp_trend': 'stable'
-                                },
-                                'firmware': 44,
-                                'last_message': 1534451898, 'last_seen': 1534451853, 'rf_status': 66, 'battery_vp': 5321,
-                                'battery_percent': 62
-                            }
-                        ]
+                        'firmware': 44,
+                        'last_message': 1534451898, 'last_seen': 1534451853, 'rf_status': 66, 'battery_vp': 5321,
+                        'battery_percent': 62
                     }
-                ],
-                'user': {
-                    'mail': 'me@gmail.com',
-                    'administrative': {
-                        'lang': 'en-US',
-                        'reg_locale': 'fi-FI',
-                        'unit': 0, 'windunit': 2,
-                        'pressureunit': 0,
-                        'feel_like_algo': 1
-                    }
-                }
+                ]
             }
+        ],
+        'user': {
+            'mail': 'me@gmail.com',
+            'administrative': {
+                'lang': 'en-US',
+                'reg_locale': 'fi-FI',
+                'unit': 0, 'windunit': 2,
+                'pressureunit': 0,
+                'feel_like_algo': 1
+            }
+        }
+    }
 
     return example_response
