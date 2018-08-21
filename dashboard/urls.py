4c2137ae5a74500b2f2ecd19108e5b52181f5efc
Wanes Missals <wanes.missals@gmail.com>
2018-08-21 22:42:26 +0300

    add material dashboard
    Demo dashboard included for template.

---
+++ dashboard/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dashboard/', TemplateView.as_view(template_name='dashboard/dashboard.html'), name='dashboard'),
+    path('icons/', TemplateView.as_view(template_name='dashboard/icons.html'), name='icons'),
+    path('map/', TemplateView.as_view(template_name='dashboard/map.html'), name='map'),
+    path('notifications/', TemplateView.as_view(template_name='dashboard/notifications.html'), name='notifications'),
+    path('tables/', TemplateView.as_view(template_name='dashboard/tables.html'), name='tables'),
+    path('typography/', TemplateView.as_view(template_name='dashboard/typography.html'), name='typography'),
+    path('upgrade/', TemplateView.as_view(template_name='dashboard/upgrade.html'), name='upgrade'),
+    path('user/', TemplateView.as_view(template_name='dashboard/user.html'), name='user'),
+]
